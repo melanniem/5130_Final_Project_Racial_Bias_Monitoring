210d8dfc24e3859ff0f91b400b6015cba973c3b0
Svenya Wilhelm <wilhelm.sv@northeastern.edu>
2026-03-10 09:30:56 -0400

    Add initial implementation of project files for racial bias quantification

---
+++ main.py
@@ -0,0 +1,7 @@
+## CS5130 Project - Quantifying Racial Bias in Automated Resume Screening: An Audit Study of Google's Gemini API ##
+
+import input
+import prompt_standardization
+import model_interface
+import data_persistence
+import bias_quantification
