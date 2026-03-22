e53bc51539a825c3fa09c5c5e742a6e174f44bec
hitika <hitikadalwadi@gmail.com>
2026-03-22 14:28:24 -0400

    added prompt standardisation and gemini_interface in main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from input_layer import input
+from input_layer import input as input_layer
 from data_persistence import data_persistence
 from model_interface import gemini_interface
 from prompt_layer import prompt_standardization
 import logging
 import pandas as pd
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+import json
 
 INPUT_PATH = "input_combinations.csv"
 LOG_DIR = Path("logs")
@@ -22,8 +25,40 @@ logger = logging.getLogger(__name__)
 
 
 if __name__ == "__main__":
-    # Load input data
-    df = input.run_input_layer()
-    print(df.head())
+    # Load resume data
+    resume_df = input_layer.run_input_layer()
+    print(resume_df.head())
+    logger.info(f"Generated {len(resume_df)} resume dataframe")
+
+    # Load prompt data
+    prompt_df = prompt_standardization.run_prompt_layer()
+    print(prompt_df.head())
+    logger.info(f"Generated {len(prompt_df)} prompts dataframe")
+
+    # Run model interface
+    load_dotenv() # Load data variables
+
+    # Initialize LLM
+    client = gemini_interface.Gemini(api_key=os.environ['GEMINI_API_KEY'])
+
+    # Scoring Resume
+    prompt_list = prompt_df.apply(lambda row: {
+        "prompt": row["prompt"],
+        "resume_id": row["resume_id"],
+        "race_group": row["identity"]
+    }, axis=1).tolist()
+
+    logger.info(f"Scoring {len(prompt_list)} prompts via Gemini...")
+    results = client.score_batch(prompt_list) # For running whole pipeline
+    #results = client.score_batch(prompt_list[:5]) # Test few prompts
+
+    logger.info(f"Scoring complete. {sum(1 for r in results if r['score'] is not None)} succeeded, "
+                f"{sum(1 for r in results if r['score'] is None)} failed.")
+
+
+
+
+
+
 
 
